bbcd5b3a481c74e6d402a78efbaeeb52eb93af58
Rakibul Islam <rakibulislammondal21@gmail.com>
2025-03-26 22:50:21 +0530

    Updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import "./css/footer.scss"
 export default function Footer() {
   return (
     <div className='footer'>
-      COPYRIGHT © 2025 Developed by RAKIBUL
+      COPYRIGHT © 2025 |  All right reserved
     </div>
   )
 }
